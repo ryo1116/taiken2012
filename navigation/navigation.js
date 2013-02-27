e86acf8a7f8f7227f7f3df2d7000fa011b12601f
Ryoichi Eguchi <ryo1103yosi@gmail.com>
2013-02-28 01:43:31 +0900

    navigation.jsの完成版をアップ

--- navigation/navigation.js
+++ navigation/navigation.js
@@ -1,60 +1,26 @@
-var start;              // 出発地
-var start_address;      // 出発地の住所
-
-var destination;        // 目的地
-var dest_address;       // 目的地の住所
-
-var latlng;             // 緯度と経度
-
-// 出発地と目的地を結ぶルートを表示させるために必要なデータ
 var directionDisplay;
 var directionsService = new google.maps.DirectionsService( );
 var map;
 
-// ---------------------------------------------------------------------------------------
+var start;              // 出発地
+var start_address;      // 出発地の住所
+var destination;        // 目的地
+var dest_address;       // 目的地の住所
+
 $(function(){
-    // 「ナビ」ボタン(リンク)がクリックされた場合
-    $('#navi').live('click', function(){
-        // 出発地を取り出す
-        // ①
-        if(start_address == "" || start_address == null) start_address = "三ノ宮駅";
-
-        // 目的地を取り出す
-		// ②
-		if(dest_address == "" || dest_address == null) dest_address = "神戸電子専門学校";
-
-        // 出発地から地図を作成する
-        getMap(start_address);
+    $('#freelink li').live('click', function(){
+        // 目的地の住所を取り出し、セッションスコープで保持しておく
+        dest_address = $('#freeaddress').attr("value");
+        destination = dest_address;
+        getMap();
     });
 
-// ---------------------------------------------------------------------------------------
-    // map(地図描画ページ)が表示される直前
-    $('#map').live('pageshow', function(){
-        // ヘッダー部に目的地をセットする
-        document.getElementById("map_destination").innerHTML = dest_address;
-
-        // 出発地と目的地の住所からルートリクエストを生成する
-        // travelModeは、ルートの計算に使用する交通手段を指定する。有効な値は次の通り
-        //  TravelMode.DRIVING　：道路網を使用した標準の運転ルート。
-        //  TravelMode.WALKING　：歩行者専用道路と歩道（使用できる場合）を使用した徒歩ルート。
-        //  TravelMode.BICYCLING：自転車パスと優先道路を使用する自転車ルート（現在は米国でのみ利用可）
-        var request = {
-            // 出発地を指定する
-            // ③
-            // 目的地を指定する
-            // ④
-            // 交通手段を指定する
-            // ⑤
-        };
-        
-        // 指定された２地点間のルートを計算する
-        directionsService.route(request, function(result, status) {
-            // 計算結果が「OK」ならば
-            if (status == google.maps.DirectionsStatus.OK) {
-                // 出発地点と到着地点にマーカーを配置し、ラインを表示する
-                // ⑥
-            }
-        });
+    // index(最初のページ)の目的地(リストアイテム)がクリックされた場合
+    $('#destination li').live('click', function(){
+        // 目的地とその住所を取り出し、セッションスコープで保持しておく
+        destination = $(this).text();
+        dest_address = $(this).attr("address");
+        getMap();
     });
 
 // ---------------------------------------------------------------------------------------
@@ -64,26 +30,62 @@ $(function(){
         document.getElementById("map_data").innerHTML = "";
     });
 
+// ---------------------------------------------------------------------------------------
+    // map(地図描画ページ)が表示される直前
+    $('#map').live('pageshow', function(){
+
+/*
+        // 出発地とその住所をセッションスコープから取り出す
+        //var map_start = sessionStorage.getItem("start");
+        var map_start_address = sessionStorage.getItem("start_address");
+
+        // 目的地とその住所をセッションスコープから取り出す
+        var map_destination = sessionStorage.getItem("destination");
+        var map_dest_address = sessionStorage.getItem("dest_address");
+*/
+        // ヘッダー部に目的地をセット
+        document.getElementById("map_destination").innerHTML =  destination;
+        //document.getElementById("map_destination").innerHTML =  map_start_address;
+
+        // 出発地と目的地の住所からルートリクエストを生成する
+        // travelModeは、ルートの計算に使用する交通手段を指定する。有効な値は次の通り
+        // TravelMode.DRIVING は道路網を使用した標準の運転ルート。
+        // TravelMode.WALKING は歩行者専用道路と歩道（使用できる場合）を使用した徒歩ルート。
+        // TravelMode.BICYCLING は自転車パスと優先道路を使用する自転車ルート（現在は米国でのみ利用可）
+        var request = {
+            origin:start_address,
+            destination:dest_address,
+            travelMode: google.maps.TravelMode.WALKING
+        };
+        
+        // 指定された２地点間のルートを計算し、ポリラインの表示と、出発地点と到着地点のマーカーを配置する。
+        directionsService.route(request, function(result, status) {
+            if (status == google.maps.DirectionsStatus.OK) {
+                directionsDisplay.setDirections(result);
+            }
+        });
+
+    });
 });
 
 // ---------------------------------------------------------------------------------------
 
-// 受け取った住所をもとに地図を準備する
-function getMap(address){
-    // 住所からジオコーダーを使って地図を生成する
+// 出発地の住所をベースに地図を準備する
+function getMap(){
+    // 出発地とその住所を取り出し、セッションスコープで保持しておく
+    start_address = $('#start').attr("value");
+
+    // 出発地住所からジオコーダーを使って地図を生成しておく
     var geocoder = new google.maps.Geocoder( );
     if (geocoder) {
-        geocoder.geocode({'address': address}, function(results, status) {
+        geocoder.geocode({'address': start_address}, function(results, status) {
             if (status == google.maps.GeocoderStatus.OK) {
                 var myOptions = {
                     zoom:17,
                     mapTypeId: google.maps.MapTypeId.ROADMAP
                 };
                 map = new google.maps.Map(document.getElementById("map_data"), myOptions);
-                latlng = results[0].geometry.location;
-                map.setCenter(latlng);
-                                
-                // 2地点間のルート表示するための準備
+                map.setCenter(results[0].geometry.location);
                 directionsDisplay = new google.maps.DirectionsRenderer( );
                 directionsDisplay.setMap(map);
             } else {
